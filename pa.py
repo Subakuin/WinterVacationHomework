909e9a26fa68d26387c346c465b2f7d898defa60
Subakuin <1048379742@qq.com>
2021-02-22 23:46:04 +0800

    pa.py

--- pa.py
+++ pa.py
@@ -7,17 +7,6 @@ from bs4 import BeautifulSoup
 headers = {
      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0'
 }
-def jieba_cloud(file_name):
-    with open(file_name, 'r', encoding='utf8') as f:
-        word_list = jieba.cut(f.read())
-
-        result = " ".join(word_list)   
-        # 制作中文词云
-        icon_name = " "
-        icon = "ciyun"
-        pic = icon + '.png'
-        gen_stylecloud(text=result, font_path='simsun.ttc', output_name=pic)
-        return pic
 def spider_comment(movie_id, page):
     comment_list = []
     with open("douban.txt", "a+", encoding='utf-8') as f:
@@ -29,13 +18,28 @@ def spider_comment(movie_id, page):
             req = requests.get(url, headers=headers)
             req.encoding = 'utf-8'
             comments = re.findall('<span class="short">(.*)</span>', req.text)
-
-
             f.writelines('\n'.join(comments))
     print(comments)
+def jieba_cloud(file_name, icon):
+    with open(file_name, 'r', encoding='utf8') as f:
+        word_list = jieba.cut(f.read())
+
+        result = " ".join(word_list)   
+        icon_name = " "
+        if icon == "1":
+            icon_name = ''
+        elif icon == "2":
+            icon_name = "fas fa-dragon"
+        pic = str(icon) + '.png'
+        if icon_name is not None and len(icon_name) > 0:
+            gen_stylecloud(text=result, icon_name=icon_name, font_path='simsun.ttc', output_name=pic)
+        else:
+            gen_stylecloud(text=result, font_path='simsun.ttc', output_name=pic)
+        return pic
+# 主函数
 if __name__ == '__main__':
     movie_id = '27619748'
     page = 10
     spider_comment(movie_id, page)
-    jieba_cloud("douban.txt")
-
+    jieba_cloud("douban.txt", "1")
+    jieba_cloud("douban.txt", "2")
